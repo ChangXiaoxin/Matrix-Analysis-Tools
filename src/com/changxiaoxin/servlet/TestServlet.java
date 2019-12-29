9ff0a6ddded88f20905a28afb90077761377f221
changxiaoxin <changx1922330593@gmail.com>
2019-12-30 01:01:27 +0800

    remove class DataSet_self and House_data

--- src/com/changxiaoxin/servlet/TestServlet.java
+++ src/com/changxiaoxin/servlet/TestServlet.java
@@ -158,37 +158,10 @@ public class TestServlet extends HttpServlet {
 		request.getRequestDispatcher("liner.jsp").forward(request, response);
 	}
 	//----------------------------------------------------------------------
-	
-	public class House_data {
-		   double house_size;
-		   double house_price;
-		   public double getHouse_size() {
-		      return house_size;
-		   }
-		   public void setHouse_size(double house_size) {
-		      this.house_size = house_size;
-		   }
-		   public double getHouse_price() {
-		      return house_price;
-		   }
-		   public void setHouse_price(double house_price) {
-		      this.house_price = house_price;
-		   }
-		}
 
-	public class DataSet_self extends HashMap<Double, House_data>{
-		   public static double number = 0;
-		   public static double getNumber() {
-		      return number;
-		   }
-		   public void addData(double house_size,double house_price){
-			   House_data housedata = new House_data();
-			   housedata.setHouse_price(house_price);
-			   housedata.setHouse_size(house_size);
-//		  	   this.put(number++, housedata);
-			   this.put(housedata.house_size, housedata);
-		   }
-	}
+	
+	
+	
 	//------------------------------------------------------------------
 	//---------------------------</线性回归>-----------------------------
 }
